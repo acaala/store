f00e7849c325d7353065e9dc9b0712c23f2f3e18
Liam W <elnewatts@gmail.com>
2021-07-10 09:57:25 +0100

    connected to backend and now displays message on template

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+import { onMount } from "svelte";
+import { dataset_dev, set_data } from "svelte/internal";
+
+
+let data: string
+
+    async function testApi() {
+        await fetch(`http://localhost:4000/api`).then((r) => r.json()).then(d => data = d.message)
+        
+        console.log(data)
+    }
+
+
+    onMount(testApi)
+</script>
+
+<h1>Welcome to SvelteStore</h1>
+<p>{data}</p>
