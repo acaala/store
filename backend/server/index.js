695ec6f7f1117a951ad327c72480bcb62cbaa658
Liam W <elnewatts@gmail.com>
2021-07-10 13:28:18 +0100

    removed depreciated body-parcer

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
 const Product = require('../models/product');
 
 const dbURI = process.env.DB_URI
@@ -16,9 +15,9 @@ const PORT = process.env.PORT || 4000;
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.get("/api/v1", (req, res) => {
     Product.find().then((result) => res.json( {products: result}));
