7884fea47a22cbcf192fe522fa563b00557a9e81
Liam W <elnewatts@gmail.com>
2021-07-10 10:18:53 +0100

    added dotenv and created key for server connection

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,4 +1,14 @@
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose')
+
+const dbURI = process.env.DB_URI
+
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => console.log('connected to db'))
 
 const PORT = process.env.PORT || 4000;
 
